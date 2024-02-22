6bef8f629dd0384b3e7fb092fc68a840d721ceaf
duong <duongtde170786@fpt.edu.vn>
2024-02-22 10:44:32 +0700

    thu

---
+++ client/src/components/home/BookCard.jsx
@@ -0,0 +1,29 @@
+const BookCard = ({ book }) => {
+    return (
+      <div>
+        <h2>{book.title}</h2>
+        <p>{book.description}</p>
+        <p>{book.price} $</p>
+        <p>Status: {book.status}</p>
+        <p>Created At: {book.createdAt}</p>
+        <p>Last Updated At: {book.lastUpdateAt}</p>
+        <p>
+          URL: <a href={book.url}>{book.url}</a>
+        </p>
+        <div>
+          <h3>Images:</h3>
+          {book.images.map((image, index) => (
+            <img key={index} src={image} alt="book" />
+          ))}
+        </div>
+        <div>
+          <h3>Types:</h3>
+          {book.types.map((type, index) => (
+            <p key={index}>{type}</p>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  export default BookCard;
+  
