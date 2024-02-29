edccb36cb0da51035ac606b9d291fa0a18a8840c
duong <duongtde170786@fpt.edu.vn>
2024-02-29 21:52:17 +0700

    use react-reader

--- client/src/pages/Reader.jsx
+++ client/src/pages/Reader.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { ReactReader } from "react-reader";
 import alice from '../assets/alice.epub';
 import guong from '../assets/Guong Thay Tro.epub';
+import Panel from "../components/epub/Panel";
 const Reader = () => {
    const [location, setLocation] = useState(null);
    const locationChanged = epubcifi =>{setLocation(epubcifi)}
   return (
     <div >
-      
+      <Panel/>
       <div style={{ height: '90vh'}}>
       <ReactReader
         url={guong}
