435769e7f8775010a66fc75359568e8d6272975e
duong <duongtde170786@fpt.edu.vn>
2024-03-07 09:55:54 +0700

    change

--- client/src/components/epub/Panel.jsx
+++ client/src/components/epub/Panel.jsx
@@ -1,11 +1,13 @@
 import View from "./Contentview";
+import Selection from "./Selection";
 import Styling from "./Styling";
 
 const Panel = () => {
     return ( 
-        <div className="">
+        <div className=" flex justify-end mr-3 space-x-2 ">
             <Styling/>
-            <View/>
+            {/* <View/> */}
+            <Selection/>
         </div>
 
      );
--- client/src/components/epub/Styling.jsx
+++ client/src/components/epub/Styling.jsx
@@ -3,7 +3,7 @@ import { Button } from "@material-tailwind/react";
 const Styling = () => {
     return ( 
         <div className="">
-
+            <Button>Styling</Button>
         </div>
      );
 }
