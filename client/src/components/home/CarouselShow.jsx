04d219a6ae1e69a94f8e3ba3a849c71b28c42ac3
enzo <hoangvietqb1912@gmail.com>
2024-03-21 10:06:16 +0700

    carousel

---
+++ client/src/components/home/CarouselShow.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+function CarouselShow({ slides }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      {/* Left Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
+        {slides.map((slide, slideIndex) => (
+          <div key={slideIndex} onClick={() => goToSlide(slideIndex)}></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CarouselShow;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,37 @@
+import CarouselShow from "../components/home/CarouselShow";
+import { FooterWithSocialLinks } from "../components/home/Footer";
 const Home = () => {
+  // //  const slides = [
+  //   "https://images7.alphacoders.com/133/1338193.png",
+  //   "https://images5.alphacoders.com/132/1326363.png",
+  //   "https://images3.alphacoders.com/167/167539.jpg",
+  //   "https://images8.alphacoders.com/424/424848.jpg",
+  // ];
+  const slides = [
+    {
+      url: "https://images7.alphacoders.com/133/1338193.png",
+    },
+    {
+      url: "https://images5.alphacoders.com/132/1326363.png",
+    },
+    {
+      url: "https://images3.alphacoders.com/167/167539.jpg",
+    },
+    {
+      url: "https://images8.alphacoders.com/424/424848.jpg",
+    },
+    {
+      url: "https://images5.alphacoders.com/455/455726.jpg",
+    },
+  ];
   return (
     <div className="w-max-[1280px] content-center h-[3000px]">
+      <div className="object-cover cursor-pointer w-full h-full rounded-[9px] ">
+        <CarouselShow slides={slides} />
+      </div>
       <div className="w-max-[1280px] h-[1400px] bg-white"></div>
       <div className="w-max-[1280px] h-[1400px] bg-black"></div>
+      <FooterWithSocialLinks />
     </div>
   );
 };
