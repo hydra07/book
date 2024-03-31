ece69a0d0bc6a545b1ba6373ba7a35557d31ae55
ngdth <thienndde170807@fpt.edu.vn>
2024-03-31 17:44:01 +0700

    new

--- client/src/app/type/[id]/page.tsx
+++ client/src/app/type/[id]/page.tsx
@@ -1,15 +1,50 @@
-import Type from "@/app/(components)/type/index";
-import axios from "../../../lib/axios";
-// eslint-disable-next-line react/display-name
-export default async ({params}: { params: { id: number } }) => {
-    const res = await axios.get(`/test/token/getType/${params.id}`)
-    const type = await res.data;
+// import axios from "../../../lib/axios";
+// import Type from "@/app/(components)/type/index"
+// // eslint-disable-next-line react/display-name
+// export default async ({params}: { params: { id: number } }) => {
+//     const res = await axios.get(`/test/token/getType/${params.id}`)
+//     const type = await res.data;
+//     if (!type){
+//         return <div>Type not found!</div>
+//     }
+//     return (
+//         <div>
+//             <Type type={type} />
+//         </div>)
+// }
 
-    if (!type){
-        return <div>Type not found!</div>
-    }
-    return (
-        <div>
-            <Type type={type} />
-        </div>)
-}
+'use client'
+import React, { useState, useEffect } from 'react';
+import axios from '../../../lib/axios';
+import Book from '@/types/book';
+import BookCard from '@/app/(components)/type/BookCard';
+
+
+const TypePage: React.FC<{ params: { id: number } }> = ({ params }) => {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    const fetchType = async () => {
+      try {
+        const res = await axios.get(`type/getBookByType`);
+        setBooks(res.data);
+        console.log(res);
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
+    };
+
+    fetchType();
+  }, [params.id]);
+
+  return (
+    <div className="container mx-auto px-4 py-8 mt-20">
+      <h2 className="text-2xl font-bold mb-4 pl-4">Type {params.id}</h2>
+      {books.map(book => (
+        <BookCard key={book.id} book={book} />
+      ))}
+    </div>
+  );
+};
+
+export default TypePage;
