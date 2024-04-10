8edd15ea000b5c05d556849bf2edba7ff9ab8698
ngdth <thienndde170807@fpt.edu.vn>
2024-04-10 10:42:18 +0700

    test

--- client/src/app/(components)/book/BookDescriptions.tsx
+++ client/src/app/(components)/book/BookDescriptions.tsx
@@ -1,51 +1,58 @@
-'use client'
-import Book from '@/types/book';
-import {Button} from '@material-tailwind/react';
-import RatingBar from './RatingBar';
-import BookAuthor from './BookAuthor';
-import {useState} from "react";
+"use client";
+import Book from "@/types/book";
+import { Button } from "@material-tailwind/react";
+import RatingBar from "./RatingBar";
+import BookAuthor from "./BookAuthor";
+import { useState } from "react";
 import Link from "next/link";
-import axios from '@/lib/axios';
 // eslint-disable-next-line react/display-name,import/no-anonymous-default-export
-export default ({book}: { book: Book }) => {
-    const [isExpanded, setIsExpanded] = useState<boolean>(false);
-    const handleReadClick = () =>{
-        axios.post(`/book/views/${book.id}`);
-        window.location.href= `/ebook/${book.id}`;
-    }
-    
-    return (
-        <div className="">
-        <div className="text-white">
-            <h1 className="text-4xl ">{book.title}</h1>
-            <BookAuthor book={book}/>
-            <span className="rate flex py-3 ">
-                    <RatingBar book={book}/>
-                </span>
-            <p className="py-3">Lượt xem: {book.views}</p>
-        </div>
-        <span className="flex flex-row my-8 space-x-2">
-
-          <div className='flex flex-row space-x-3 whitespace-nowrap'>
-              <p className="text-white"> Loại sách: </p>
-              {book.types!.map((type, index) => (
-                  // eslint-disable-next-line react/jsx-key
-                  <Link href={`/type/${type.id}`} key={index}
-                        className="opacity-70 hover:opacity-100 text-white ">{type.name}</Link>))}
-              {/*<a href="/" key={index}*/}
-              {/*   className="p-2 text-white  rounded-lg bg-blue-gray-500 hover:bg-blue-gray-800">{type.name}</a>))}*/}
-          </div>    
+export default ({ book }: { book: Book }) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const handleClick = () =>{
+    window.location.href= `/ebook/${book.id}` ;
+  }
+  return (
+    <div className="">
+      <div className="text-white">
+        <h1 className="text-4xl ">{book.title}</h1>
+        <BookAuthor book={book} />
+        <span className="rate flex py-3 ">
+          <RatingBar book={book} />
         </span>
-            {/* <ButtonRead book={book} /> */}
-        <Button placeholder={null} onClick={handleReadClick}> Read </Button>
-        <Button placeholder={null} className="ml-4"> Follow </Button>
-        <div className="pt-8 flex space-x-1 ">
-            <p className="text-white  text-md">{isExpanded ? book.description : `${book.description?.substring(0, 100)}`}</p>
-            <button className="bg-none text-green-400 text-nowrap self-end" onClick={() => setIsExpanded(!isExpanded)}>
-                {isExpanded ? 'Thu gọn' : 'Xem thêm'}
-            </button>
+      </div>
+      <span className="flex flex-row my-8 space-x-2">
+        <div className="flex flex-row space-x-3 whitespace-nowrap">
+          <p className="text-white"> Loại sách: </p>
+          {book.types!.map((type, index) => (
+            // eslint-disable-next-line react/jsx-key
+            <Link
+              href={"/"}
+              key={index}
+              className="opacity-70 hover:opacity-100 text-white "
+            >
+              {type.name}
+            </Link>
+          ))}
+          {/*<a href="/" key={index}*/}
+          {/*   className="p-2 text-white  rounded-lg bg-blue-gray-500 hover:bg-blue-gray-800">{type.name}</a>))}*/}
         </div>
-        {/* <BookComment book={book} /> */}
+      </span>
+      {/* <ButtonRead book={book} /> */}
+      <Button placeholder={null} onClick={handleClick} > Read </Button>
+      <div className="pt-8 flex space-x-1 ">
+        <p className="text-white  text-md">
+          {isExpanded
+            ? book.description
+            : `${book.description?.substring(0, 100)}`}
+        </p>
+        <button
+          className="bg-none text-green-400 text-nowrap self-end"
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          {isExpanded ? "Thu gọn" : "Xem thêm"}
+        </button>
+      </div>
+      {/* <BookComment book={book} /> */}
     </div>
-    );
+  );
 };
--- client/src/app/(components)/book/RatingBar.tsx
+++ client/src/app/(components)/book/RatingBar.tsx
@@ -1,27 +1,24 @@
 'use client';
 import Book from '@/types/book';
 import { Rating, Typography } from '@material-tailwind/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 export default ({book}:{book:Book}) => {
   const [rating,setRating] = useState<number>(book.rating!);
   const [rated, setRated] = useState(0);
   const [reviewer, setReviewer] = useState(book.reviews);
-  useEffect (()=>{
-    console.log(rated);
-  },[rated])
-  
-  
+  console.log(rated);
   return (
     <div>
       <div className="flex flex-row items-center gap-2 font-bold text-gray-400">
         <div className=''>
-          {rated}   
+          {rated}.0   
         </div>
         <Rating
           value={Math.round(rating)}
           onChange={(value) => setRated(value)}
           placeholder={null}
         />
+        
         <Typography
           color="blue-gray"
           className="font-medium text-gray-400"
