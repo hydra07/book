70219c75bf01a4a4f4e641dad6243cf051521f11
hydra07 <abcxyzkhong@gmail.com>
2024-04-12 21:03:17 +0700

    update: fontend comment

--- client/src/app/bookdetail/[id]/page.tsx
+++ client/src/app/bookdetail/[id]/page.tsx
@@ -5,12 +5,38 @@ import Breadcrumbs from '@/app/(components)/book/Breadcrumbs';
 
 import axios from '@/lib/axios';
 import Book from '@/types/book';
-export default async ({ params }: { params: { id: number } }) => {
-  let book: Book | null = null;
+
+import CommentComponent, { Comment } from '@/app/(components)/comment';
+const fetchComment = async (book: Book | null) => {
+  if (!book) return [];
   try {
-    const res = await axios.post(`/book/find/${params.id}`);
-    book = res.data;
-  } catch (error) {}
+    const res = await axios.get(`/book/getAllCommentByBook/${book.id}`);
+    const comments: Comment[] = res.data.map((comment: Comment) => ({
+      id: comment.id,
+      content: comment.content,
+      user: comment.user,
+      parent: comment.parent,
+      left: comment.left,
+      right: comment.right,
+      createdAt: comment.createdAt,
+    }));
+    return await comments;
+  } catch (error) {
+    return [];
+  }
+};
+const fetchBook = async (id: any) => {
+  try {
+    const res = await axios.post(`/book/find/${id}`);
+    return await res.data;
+  } catch (error) {
+    return null;
+  }
+};
+
+export default async ({ params }: { params: { id: number } }) => {
+  const book = await fetchBook(params.id);
+  const comments = await fetchComment(book);
   if (!book) {
     return <ErrorHandle message={`Không tìm thấy book với id:${params.id}`} />;
   } else {
@@ -18,11 +44,17 @@ export default async ({ params }: { params: { id: number } }) => {
       <div className="pt-20">
         <Breadcrumbs book={book} />
         <div className="flex flex-row p-5">
-          <div className="basis-1/3">
+          <div className="basis-1/3 sticky">
             <BookImage book={book} />
           </div>
           <div className=" pl-4 basis-2/3">
-            <BookDescriptions book={book} />
+            <div className="flex flex-col">
+              <BookDescriptions book={book} />
+              <CommentComponent
+                // comments={comments} 
+                book={book} 
+              />
+            </div>
           </div>
         </div>
       </div>
