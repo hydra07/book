daa4326ad8f3d73f38a00a33b13c834d5a84b88e
ngdth <thienndde170807@fpt.edu.vn>
2024-04-13 17:06:15 +0700

    update home

--- client/src/app/(components)/home/index.tsx
+++ client/src/app/(components)/home/index.tsx
@@ -3,29 +3,30 @@ import Book, { Type } from "@/types/book";
 import TypeBook from "./TypeBook";
 import { getAllTypesFromBooks, getBookByType } from '@/utils/sort.utils';
 import { useState } from "react";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Carousel from "./Carousel";
+import Footer from "./Footer";
 
 export default ({ listBook }: { listBook: Book[] }) => {
-	const [books, setBooks] = useState<Book[]>(listBook);
-	const [types, setTypes] = useState<Type[]>(getAllTypesFromBooks(listBook!));
-	console.log('from index.tsx')
-	return (
-		<div>
-			<div className="h-auto w-screen">
-				<div className="pt-20">
-					{types?.map((type) => {
-						return (
-							<TypeBook
-								type={type.name}
-								books={getBookByType(listBook!, type)}
-								key={type.id}
-							/>
-						);
-					})}
-				</div>
-			</div>
-			<ToastContainer />
-		</div>
-	);
+  const [books, setBooks] = useState<Book[]>(listBook);
+  const [types, setTypes] = useState<Type[]>(getAllTypesFromBooks(listBook!));
+  console.log('from index.tsx')
+  return (
+    <div>
+      <Carousel />
+      <div className="h-auto w-screen">
+        <div className="pt-20">
+          {types?.map((type) => {
+            return (
+              <TypeBook
+                type={type.name}
+                books={getBookByType(listBook!, type)}
+                key={type.id}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
